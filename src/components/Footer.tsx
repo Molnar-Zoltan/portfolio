63a5c52211bacfdcc0aa02ae310e0ea73b988f9c
Molnar-Zoltan <weperdev@gmail.com>
2025-08-10 07:57:24 +0200

    Update footer styling and move background to CSS
    Removed the background color from the Footer component and added footer-specific background and border styles to globals.css. This centralizes styling in CSS and improves maintainability.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     const currentYear = new Date().getFullYear();
 
     return (
-        <footer className="bg-neutral-900 min-h-20 flex items-center justify-center text-gray-100 font-semibold text-sm">
+        <footer className="min-h-20 flex items-center justify-center text-gray-100 font-semibold text-sm">
             <p>&copy; {currentYear} Zoltán Molnár</p>
         </footer>
     );
