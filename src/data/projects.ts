dfd236aaa248f159f87ebdfc0f7bdc74341084bf
Zoltán Molnár <weperdev@gmail.com>
2025-11-21 10:23:30 +0100

    Add Best Shop project
    Added the Best Shop project with image and links to the projects list.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,6 +7,12 @@ export interface Project {
 
 export const projects: Project[] = [
     { 
+        name: "Best Shop", 
+        image: "/images/projects/best-shop.png", 
+        githubLink: "https://github.com/Molnar-Zoltan/best-shop-capstone-project/", 
+        liveLink: "https://molnar-zoltan.github.io/best-shop-capstone-project/" 
+    },
+    { 
         name: "Little Lemon - Reserve a Table", 
         image: "/images/projects/little-lemon-reserve-a-table.png", 
         githubLink: "https://github.com/Molnar-Zoltan/little-lemon-reserve-a-table/", 
@@ -29,11 +35,5 @@ export const projects: Project[] = [
         image: "/images/projects/currency-converter.png", 
         githubLink: "https://github.com/Molnar-Zoltan/currency-converter", 
         liveLink: "https://molnar-zoltan.github.io/currency-converter/" 
-    },
-    { 
-        name: "Simple Calculator", 
-        image: "/images/projects/simple-calculator.png", 
-        githubLink: "https://github.com/Molnar-Zoltan/simple-calculator", 
-        liveLink: "https://molnar-zoltan.github.io/simple-calculator/" 
     },
 ];
